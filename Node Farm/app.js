82c6b12780bb408b6825195facc028078d0562aa
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2023-12-28 11:55:37 -0700

    added api endpoint

--- Node Farm/app.js
+++ Node Farm/app.js
@@ -1,12 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+let productData;
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-
   if (url === "/overview" || url === "/") {
+    console.log(productData);
     res.end("This is the OVERVIEW");
   } else if (url === "/products") {
     res.end("This is the PRODUCTS");
+  } else if (url === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

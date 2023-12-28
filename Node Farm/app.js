86681236f5ab0498205051f1fecc2b0b4b11af9b
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2023-12-28 14:43:31 -0700

    used the html template to display overview page on empty or overview route

--- Node Farm/app.js
+++ Node Farm/app.js
@@ -1,22 +1,43 @@
 const http = require("http");
 const fs = require("fs");
-let productData;
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const cardTemplate = fs
+  .readFileSync(`${__dirname}/templates/template-card.html`)
+  .toString();
+
+const overiewTemplate = fs
+  .readFileSync(`${__dirname}/templates/template-overview.html`)
+  .toString();
+const products = JSON.parse(data);
+
+const productCards = products.map((product) => {
+  let template = cardTemplate;
+  template = template.replaceAll(/{%IMAGE%}/g, product.image);
+  template = template.replaceAll(/{%PRODUCT_NAME%}/g, product.productName);
+  template = template.replaceAll(/{%QUANTITY%}/g, product.quantity);
+  template = template.replaceAll(/{%PRICE%}/g, product.price);
+  if (!product.organic) {
+    template = template.replaceAll(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return template;
+});
+
+console.log(productCards[0]);
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/overview" || url === "/") {
-    console.log(productData);
-    res.end("This is the OVERVIEW");
+    overviewTemplate = overiewTemplate.replaceAll(
+      /{%PRODUCT_CARDS%}/g,
+      productCards.reduce((accum, curr) => accum + curr, "")
+    );
+    res.setHeader("Content-type", "text/html");
+    res.end(overviewTemplate);
   } else if (url === "/products") {
     res.end("This is the PRODUCTS");
   } else if (url === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, (err, data) => {
-      const productData = JSON.parse(data);
-      console.log(productData);
-
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

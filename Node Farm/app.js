596b2cd423544ab9feedc0ca75da5a4936db0c3c
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2023-12-28 20:30:16 -0700

    refactored html generation

--- Node Farm/app.js
+++ Node Farm/app.js
@@ -1,38 +1,30 @@
 const http = require("http");
 const fs = require("fs");
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+
+// Templates
 const cardTemplate = fs
   .readFileSync(`${__dirname}/templates/template-card.html`)
   .toString();
 
-const overiewTemplate = fs
+const overviewTemplate = fs
   .readFileSync(`${__dirname}/templates/template-overview.html`)
   .toString();
 const products = JSON.parse(data);
 
 const productCards = products.map((product) => {
-  let template = cardTemplate;
-  template = template.replaceAll(/{%IMAGE%}/g, product.image);
-  template = template.replaceAll(/{%PRODUCT_NAME%}/g, product.productName);
-  template = template.replaceAll(/{%QUANTITY%}/g, product.quantity);
-  template = template.replaceAll(/{%PRICE%}/g, product.price);
-  if (!product.organic) {
-    template = template.replaceAll(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return template;
+  return replaceTemplate(cardTemplate, product);
 });
-
-console.log(productCards[0]);
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/overview" || url === "/") {
-    overviewTemplate = overiewTemplate.replaceAll(
+    const overview = overviewTemplate.replaceAll(
       /{%PRODUCT_CARDS%}/g,
       productCards.reduce((accum, curr) => accum + curr, "")
     );
     res.setHeader("Content-type", "text/html");
-    res.end(overviewTemplate);
+    res.end(overview);
   } else if (url === "/products") {
     res.end("This is the PRODUCTS");
   } else if (url === "/api") {
@@ -47,3 +39,19 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+function replaceTemplate(template, product) {
+  let output = template.replaceAll(/{%IMAGE%}/g, product.image);
+  output = output.replaceAll(/{%PRODUCT_NAME%}/g, product.productName);
+  output = output.replaceAll(/{%QUANTITY%}/g, product.quantity);
+  output = output.replaceAll(/{%PRICE%}/g, product.price);
+  if (!product.organic) {
+    output = output.replaceAll(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  output = output.replaceAll(/{%FROM%}/g, product.from);
+  output = output.replaceAll(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replaceAll(/{%ID%}/g, product.id);
+  output = output.replaceAll(/{%DESCRIPTION%}/g, product.description);
+
+  return output;
+}

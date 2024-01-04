686b99dcbd74e44035320abaecb87d21ff477c82
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-04 15:14:01 -0700

    added a user route and general route

--- Express Basics/app.js
+++ Express Basics/app.js
@@ -2,13 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the first middleware");
-  next();
+// Two middlewares that log to the console
+// app.use((req, res, next) => {
+//   console.log("In the first middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("In the second middleware");
+//   res.send("<h1>All middlewares complete</h1>");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Welcome to the users page</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("In the second middleware");
-  res.send("<h1>All middlewares complete</h1>");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to the homepage!</h1>");
 });
 app.listen(3000);
